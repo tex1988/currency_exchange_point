72e08d4ccaeedfe7a47bf46a90086f571225decf
Oleksii Zinkevych <zinkevich-as@ukr.net>
2023-03-26 16:06:13 +0300

    Added gcloud configuration

--- src/main/java/com/github/oleksii/zinkevych/currency_exchange_point/CurrencyExchangePointApplication.java
+++ src/main/java/com/github/oleksii/zinkevych/currency_exchange_point/CurrencyExchangePointApplication.java
@@ -2,20 +2,17 @@ package com.github.oleksii.zinkevych.currency_exchange_point;
 
 import java.util.Currency;
 
-import com.github.oleksii.zinkevych.currency_exchange_point.service.common.BankService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.format.support.FormattingConversionService;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
